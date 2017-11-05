ebf6c1ff3432219caba3b1a2c25f2b2695f85c81
Jem Hilton <jem.hilton@homechef.com>
2017-11-04 23:31:33 -0500

    Add a few more API wrappers

--- app.js
+++ app.js
@@ -29,6 +29,20 @@ const getProducts = () => {
 };
 
 /**
+ * @function  [getCoinbaseAccounts]
+ * @returns {Json} coinbaseAccounts
+ */
+const getCoinbaseAccounts = () => {
+  authedClient.getCoinbaseAccounts()
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.log(error)
+  });
+};
+
+/**
  * @function  [getAccounts]
  * @returns {Json} accounts
  */
@@ -47,19 +61,71 @@ const getAccounts = () => {
  * @returns {Json} account
  */
 const getAccount = (currencyType) => {
-  const accountId = sandbox ? process.env["SANDBOX_${currencyType}"] : process.env[currencyType]
+  return new Promise((resolve, reject) => {
+    const accountId = sandbox ? process.env[`SANDBOX_${currencyType}`] : process.env[currencyType]
 
-  if(accountId) {
-    authedClient.getAccount(accountId)
-    .then(data => {
-      console.log(data)
-    })
-    .catch(error => {
-      console.log(error)
+    if (accountId) {
+      authedClient.getAccount(accountId)
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.log(error)
+      });
+    } else {
+      throw 'no currency matches that type'
+    }
+  })
+}
+
+const getCoinbaseAccount = (currencyType) => {
+  return new Promise((resolve, reject) => {
+    const findCoinbaseAccount = (coinbaseAccount) => {
+      return coinbaseAccount.currency === currencyType;
+    }
+
+    authedClient.getCoinbaseAccounts()
+      .then(data => {
+        const account = data.find(findCoinbaseAccount)
+        console.log(account)
+        return account;
+      })
+  })
+}
+
+const getAccountHistory = (currencyType) => {
+  getAccount(currencyType)
+    .then(account => {
+      authedClient.getAccountHistory(account.id)
+        .then(data => {
+          console.log(data)
+        })
+        .catch(error => {
+          console.log(error)
+        });
     });
-  } else {
-    throw 'no currency matches that type'
-  }
 };
+
+// Deposit to your Exchange USD account from your Coinbase USD account.
+const depositUSD = (amount) => {
+  getCoinbaseAccount('USD')
+    .then(account => {
+
+      const depositParamsUSD = {
+        'amount': amount,
+        'currency': 'USD',
+        'coinbase_account_id': account.id, // USD Coinbase Account ID
+      };
+
+      authedClient.deposit(depositParamsUSD)
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.log(error)
+      });
+    });
+};
+
 // Export all methods
-module.exports = { getProducts, getAccounts, getAccount };
+module.exports = { getProducts, getAccounts, getAccountHistory, getCoinbaseAccount, getCoinbaseAccounts, getAccount, depositUSD };
--- gdax.js
+++ gdax.js
@@ -1,7 +1,7 @@
 const program = require('commander');
 
 // Require app.js file and extract controller functions using JS destructuring assignment
-const { getProducts, getAccounts, getAccount } = require('./app');
+const { getProducts, getAccounts, getCoinbaseAccounts, getCoinbaseAccount, getAccount, getAccountHistory, depositUSD } = require('./app');
 
 program
   .version('0.0.1')
@@ -19,8 +19,28 @@ program
   .action(name => getAccounts());
 
 program
+  .command('getCoinbaseAccounts')
+  .description('Get Coinbase accounts')
+  .action(name => getCoinbaseAccounts());
+
+program
+  .command('getCoinbaseAccount')
+  .description('Get Coinbase account for currency')
+  .action(currencyType => getCoinbaseAccount(currencyType));
+
+program
   .command('getAccount')
   .description('Get account for currency')
   .action(currencyType => getAccount(currencyType));
+
+program
+  .command('getAccountHistory')
+  .description('Get account history for currency')
+  .action(currencyType => getAccountHistory(currencyType));
+
+program
+  .command('depositUSD')
+  .description('Deposit USD from coinbase account')
+  .action(amount => depositUSD(amount));
 
 program.parse(process.argv);
